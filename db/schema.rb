b969249925b0befbdfeaa0731dec4fbe9cee1a7d
Eden Lan <lanailin4@gmail.com>
2022-05-30 18:25:52 +0200

    complete geocoding stuff

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_30_123011) do
+ActiveRecord::Schema.define(version: 2022_05_30_150403) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "destinations", force: :cascade do |t|
     t.text "description"
-    t.string "name"
+    t.string "address"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.float "latitude"
+    t.float "longitude"
   end
 
   create_table "reviews", force: :cascade do |t|

1fa47fb036df766a283baed7eaa102223d5ae0a0
ConnorFord <fordc9191@gmail.com>
2022-06-01 17:42:44 +0200

    Committing jsut so we can push to heroku. Nothing is finished

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,33 @@
 class ReviewsController < ApplicationController
+
+  def new
+  end
+
+  def index
+    @reviews = Reviews.all
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @review.user = current_user
+    @travel_plan = TravelPlan.find(params[:travel_plan_id])
+    @review.travel_plan.destination = @destination
+    if @review.save
+      redirect_to destination_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+    redirect_to destination_path
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:rating, :comment)
+  end
 end

7a5d4238431f25b7e9e7f64fac12166f11a97a3c
Dina Hansen <dina.erika.hansen@gmail.com>
2022-06-06 15:20:24 +0200

    Tried to fix photo part 1

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -3,6 +3,8 @@ require 'net/http'
 require 'openssl'
 require 'json'
 require 'digest'
+require "open-uri"
+require "nokogiri"
 
 
 class DestinationsController < ApplicationController
@@ -184,8 +186,22 @@ class DestinationsController < ApplicationController
   end
 
   def show
+    # -------- This section is for displaying image of city. Image is scraped from Wikipedia ----------
+
+    url = "https://no.wikipedia.org/wiki/Toledo"
+
+    html_file = URI.open(url).read
+    html_doc = Nokogiri::HTML(html_file)
+    html_wikipedia = html_doc.search(".image img")[0]
+    #  Picks the next picture if the first picture is a book:
+    if "Question book-new.svg" == html_wikipedia.attribute("alt").value
+      html_wikipedia = html_doc.search(".image img")[1]
+    end
+    @image_link_city = html_wikipedia.attribute("src")
+
+    # ------------- Code to display each recommendation----------
     @travel_plan_new = TravelPlan.new
-    @destination = Destination.find(params[:id])
+
     longitude = @destination.longitude
     latitude = @destination.latitude
     if @destination.recommendations.empty?
